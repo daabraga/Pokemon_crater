91429538a005174d8af7fccdb788e426af9d0e8c
Diego Braga <daabraga@gmail.com>
2020-10-13 21:21:59 -0300

    initializing attack function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
+import getRndInteger from './utils/getRndInteger'
+
 const pokemons = {
     charmander: {
+        hp: 100,
+        sp: 50,
         atks: {
             ember: {
                 minDmg: 4,
                 maxDmg: 8,
-                sp: 1
+                sp: 1,
+                type: 'fire'
             },
         },
         type: 'fire',
+        hit(atk, target) {
+            const dmg = getRndInteger(atk.minDmg, atk.maxDmg) // elaboracao do dano
+            this.sp -= atk.sp // gasto de mana
+            target.hp -= dmg // hit no pokemon alvo
+        }
     },
 
     squirtle: {
@@ -32,6 +42,10 @@ const pokemons = {
         type: 'grass'
     },
     
+}
+
+const doAtk = (atk, target) => {
+
 }
 
 console.log(pokemons.bulbassaur)
